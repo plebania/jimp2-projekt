0bf17b458e6fd62286e2553018903c4e4f25c64d
Marcin Scislowski <cokolwiek@c.com>
2022-04-03 19:59:00 +0200

    poprawka obj i bin

--- src/main.c
+++ src/main.c
@@ -12,22 +12,5 @@ int main(int argc, char *argv[])
 {
 
     srand(time(NULL));
-    arg_parse(argc, argv);
-    // struct graf *g = stworz_graf(3, 3, -1, 1, 1);
-
-    // for (int x = 0; x < g->cells; x++)
-    // {
-    //     printf(" ");
-    //     for (int y = 0; y < g->tab[x]->cells; y++)
-    //         printf("[%d-%lf->%d]\n", x, g->tab[x]->tab[y]->waga, g->tab[x]->tab[y]->_do);
-    //     printf(" \n");
-    // }
-
-    // free_graf(g);
-    // double w_min = argc > 4 ? atof(argv[5]) : 0.0;
-    // double w_max = argc > 5 ? atof(argv[6]) : 1.0;
-
-    // printf("%s %s %s\n", argv[0], argv[1], argv[2]);
-    // int q = strcmp(argv[1], "make") == 0 ? 1 : 2;
-    // printf("%d", q);
+    return arg_parse(argc, argv);
 }
