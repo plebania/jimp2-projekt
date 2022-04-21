505dfc6e97a0514dc2d52fc924b17fed9b6ed18f
Plebania <plebaniahobbitanow@gmail.com>
2022-04-21 18:05:30 +0200

    Poprawki na ostatnia chwile

--- inc/arg_pars.h
+++ inc/arg_pars.h
@@ -3,8 +3,8 @@
 
 int czy_double(char *napis);
 int czy_int(char *napis);
-void zachowanie_make(FILE *plik, int w, int h, double min_wag, double max_wag);
-void zachowanie_path(FILE *plik, int from_x, int from_y, int to_x, int to_y);
+void zachowanie_make(FILE *plik, int w, int h, double min_wag, double max_wag, unsigned int psuc);
+void zachowanie_path(FILE *plik, int from, int to);
 void zachowanie_check(FILE *plik);
 
 int arg_parse(int argc, char *argv[]);
--- src/arg_pars.c
+++ src/arg_pars.c
@@ -54,21 +54,20 @@ int czy_int(char *napis)
     return 1;
 }
 
-void zachowanie_make(FILE *plik, int w, int h, double min_wag, double max_wag)
+void zachowanie_make(FILE *plik, int w, int h, double min_wag, double max_wag, unsigned int psuc)
 {
-    struct graf *g = stworz_graf(w, h, min_wag, max_wag, 0);
+    struct graf *g = stworz_graf(w, h, min_wag, max_wag, psuc);
     g->w = w;
     g->h = h;
     zapisz_graf(plik, g);
     free_graf(g);
 }
 
-void zachowanie_path(FILE *plik, int from_x, int from_y, int to_x, int to_y)
+void zachowanie_path(FILE *plik, int from, int to)
 {
     struct graf *g = wczytaj_graf(plik);
     if (g == NULL)
         return;
-    int from = from_x + from_y * g->w, to = to_x + to_y * g->w;
 
     struct dijkstra_out *out = dijkstra(g, from);
     printf("Długość: %lf\nDroga: %d", out->droga[to], to);
@@ -103,24 +102,64 @@ void zachowanie_check(FILE *plik)
     free_bfs_out(out);
 }
 
-void parse_argumenty_make(int argc, char *argv[], int od, double *min_wag, double *max_wag)
+int parse_argumenty_make(int argc, char *argv[], int od, double *min_wag, double *max_wag, unsigned int *psuc)
 {
-    if (argc == od + 3 && czy_double(argv[od + 1]) && czy_double(argv[od + 2]))
+
+    if (argc >= od + 3)
     {
-        *min_wag = atof(argv[5]);
-        *max_wag = atof(argv[6]);
-        if (min_wag > max_wag)
+        if (czy_double(argv[od + 1]) && czy_double(argv[od + 2]))
         {
-            double pom = *min_wag;
-            *min_wag = *max_wag;
-            *max_wag = pom;
+            *min_wag = atof(argv[od + 1]);
+            *max_wag = atof(argv[od + 2]);
+            if (min_wag > max_wag)
+            {
+                double pom = *min_wag;
+                *min_wag = *max_wag;
+                *max_wag = pom;
+            }
+            if (argc > od + 3 && czy_int(argv[od + 3]))
+                *psuc = atoi(argv[od + 3]);
+        }
+        else
+        {
+            for (int x = od + 1; x < argc; x += 2)
+            {
+                if (strcmp(argv[x], "-w_min") == 0 || strcmp(argv[x], "--weight_min") == 0)
+                {
+                    if (czy_double(argv[x + 1]))
+                        *min_wag = atof(argv[x + 1]);
+                    else
+                    {
+                        printf("błąd, po %s wymagana wartość parametru typu double", argv[x]);
+                        return 1;
+                    }
+                }
+                else if (strcmp(argv[x], "-w_max") == 0 || strcmp(argv[x], "--weight_max") == 0)
+                {
+                    if (czy_double(argv[x + 1]))
+                        *max_wag = atof(argv[x + 1]);
+                    else
+                    {
+                        printf("błąd, po %s wymagana wartość parametru typu double", argv[x]);
+                        return 1;
+                    }
+                }
+                else if (strcmp(argv[x], "-p") == 0 || strcmp(argv[x], "--psuc") == 0)
+                {
+                    *psuc = 1;
+                    x--;
+                }
+                else
+                {
+                    printf("błąd, nieznany argument\n");
+                    return 1;
+                }
+            }
         }
     }
-    else if (argc == od + 5 && (strcmp(argv[od + 1], "--weight_min") == 0 || strcmp(argv[od + 1], "-w_min") == 0) && (strcmp(argv[od + 3], "--weight_max") == 0 || strcmp(argv[od + 3], "-w_max") == 0) && czy_double(argv[od + 2]) && czy_double(argv[od + 4]))
-    {
-        *min_wag = atof(argv[6]);
-        *max_wag = atof(argv[8]);
-    }
+    else if (argc > od + 1 && (strcmp(argv[od + 1], "--psuc") == 0 || strcmp(argv[od + 1], "-p") == 0))
+        *psuc = 1;
+    return 0;
 }
 
 int parse_make(int argc, char *argv[])
@@ -141,6 +180,7 @@ int parse_make(int argc, char *argv[])
         return 1;
     }
     double min_wag = 0, max_wag = 1;
+    unsigned int psuc = 0;
     // polecenie postaci graph make plik [arg]... val val
     if (czy_int(argv[3]))
     {
@@ -148,7 +188,7 @@ int parse_make(int argc, char *argv[])
         {
             w = atoi(argv[3]);
             h = atoi(argv[4]);
-            parse_argumenty_make(argc, argv, 4, &min_wag, &max_wag);
+            parse_argumenty_make(argc, argv, 4, &min_wag, &max_wag, &psuc);
         }
         else
         {
@@ -168,7 +208,7 @@ int parse_make(int argc, char *argv[])
             {
                 w = atoi(argv[4]);
                 h = atoi(argv[6]);
-                parse_argumenty_make(argc, argv, 6, &min_wag, &max_wag);
+                parse_argumenty_make(argc, argv, 6, &min_wag, &max_wag, &psuc);
             }
             else
             {
@@ -185,7 +225,7 @@ int parse_make(int argc, char *argv[])
             {
                 w = atoi(argv[4]);
                 h = atoi(argv[6]);
-                parse_argumenty_make(argc, argv, 6, &min_wag, &max_wag);
+                parse_argumenty_make(argc, argv, 6, &min_wag, &max_wag, &psuc);
             }
             else
             {
@@ -210,7 +250,7 @@ int parse_make(int argc, char *argv[])
         instrukcja();
         return 1;
     }
-    zachowanie_make(out, w, h, min_wag, max_wag);
+    zachowanie_make(out, w, h, min_wag, max_wag, psuc);
     fclose(out);
     return 0;
 }
@@ -230,21 +270,19 @@ int parse_check(char *argv[])
 
 int parse_path(int argc, char *argv[])
 {
-    int from_x, from_y, to_x, to_y;
+    int from, to;
     FILE *in = fopen(argv[2], "r");
     if (!in)
     {
         fprintf(stderr, "nie udało się wczytać pliku wejściowego\n");
         return 1;
     }
-    if (argc == 7)
+    if (argc == 5)
     {
-        if (czy_int(argv[3]) && czy_int(argv[4]) && czy_int(argv[5]) && czy_int(argv[6]))
+        if (czy_int(argv[3]) && czy_int(argv[4]))
         {
-            from_x = atoi(argv[3]);
-            from_y = atoi(argv[4]);
-            to_x = atoi(argv[5]);
-            to_y = atoi(argv[6]);
+            from = atoi(argv[3]);
+            to = atoi(argv[4]);
         }
         else
         {
@@ -254,69 +292,26 @@ int parse_path(int argc, char *argv[])
             return 1;
         }
     }
-    else if (argc == 11)
+    else if (argc == 7)
     {
-        if (czy_int(argv[4]) && czy_int(argv[6]) && czy_int(argv[8]) && czy_int(argv[10]))
+        if (czy_int(argv[4]) && czy_int(argv[6]))
         {
-            int byl_from_x = 0, byl_from_y = 0, byl_to_x = 0, byl_to_y = 0;
-            for (int x = 3; x < 11; x += 2)
+            if (strcmp(argv[3], "from") == 0 && strcmp(argv[5], "to") == 0)
             {
-                if (strcmp(argv[x], "from_x") == 0)
-                {
-                    if (byl_from_x)
-                    {
-                        printf("błąd, wymagane argumenty from_x, from_y, to_x, to_y, bez powtórzeń\n");
-                        fclose(in);
-                        instrukcja();
-                        return 1;
-                    }
-                    byl_from_x = 1;
-                    from_x = atoi(argv[x + 1]);
-                }
-                else if (strcmp(argv[x], "from_y") == 0)
-                {
-
-                    if (byl_from_y)
-                    {
-                        printf("błąd, wymagane argumenty from_x, from_y, to_x, to_y, bez powtórzeń\n");
-                        fclose(in);
-                        instrukcja();
-                        return 1;
-                    }
-                    byl_from_y = 1;
-                    from_y = atoi(argv[x + 1]);
-                }
-                else if (strcmp(argv[x], "to_x") == 0)
-                {
-                    if (byl_to_x)
-                    {
-                        printf("błąd, wymagane argumenty from_x, from_y, to_x, to_y, bez powtórzeń\n");
-                        fclose(in);
-                        instrukcja();
-                        return 1;
-                    }
-                    byl_to_x = 1;
-                    to_x = atoi(argv[x + 1]);
-                }
-                else if (strcmp(argv[x], "to_y") == 0)
-                {
-                    if (byl_to_y)
-                    {
-                        printf("błąd, wymagane argumenty from_x, from_y, to_x, to_y, bez powtórzeń\n");
-                        fclose(in);
-                        instrukcja();
-                        return 1;
-                    }
-                    byl_to_y = 1;
-                    to_y = atoi(argv[x + 1]);
-                }
-                else
-                {
-                    printf("błąd, wymagane argumenty from_x, from_y, to_x, to_y, bez powtórzeń\n");
-                    fclose(in);
-                    instrukcja();
-                    return 1;
-                }
+                from = atoi(argv[4]);
+                to = atoi(argv[6]);
+            }
+            else if (strcmp(argv[3], "to") == 0 && strcmp(argv[5], "from") == 0)
+            {
+                to = atoi(argv[4]);
+                from = atoi(argv[6]);
+            }
+            else
+            {
+                printf("błąd, niepoprawne parametry wywołania\n");
+                fclose(in);
+                instrukcja();
+                return 1;
             }
         }
         else
@@ -334,7 +329,7 @@ int parse_path(int argc, char *argv[])
         instrukcja();
         return 1;
     }
-    zachowanie_path(in, from_x, from_y, to_x, to_y);
+    zachowanie_path(in, from, to);
     fclose(in);
     return 0;
 }
